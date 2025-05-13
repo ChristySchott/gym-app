38e218567c73ed4f5ac6e2eff77ed88734d11978
ChristySchott <christyhauschild@gmail.com>
2025-05-13 18:22:45 -0300

    fix: import path

--- src/http/controllers/register.controller.ts
+++ src/http/controllers/register.controller.ts
@@ -3,7 +3,7 @@ import { z } from 'zod'
 
 import { RegisterUseCase } from '@/use-cases/register.js'
 import { UserAlreadyExistsError } from '@/use-cases/errors/user-already-exists.error.js'
-import { PrismaUsersRepository } from '@/repositories/prisma-users.repository.js'
+import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users.repository.js'
 
 export async function register(request: FastifyRequest, reply: FastifyReply) {
   const registerBodySchema = z.object({
