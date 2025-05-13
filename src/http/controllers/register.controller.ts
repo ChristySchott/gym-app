e584ea2d9076d1f3008a1b999d73955e94512bb2
ChristySchott <christyhauschild@gmail.com>
2025-05-13 09:16:04 -0300

    feat: encrypt password

--- src/http/controllers/register.controller.ts
+++ src/http/controllers/register.controller.ts
@@ -1,4 +1,5 @@
 import { FastifyReply, FastifyRequest } from 'fastify'
+import { hash } from 'bcryptjs'
 import { z } from 'zod'
 
 import { prisma } from '@/lib/prisma.js'
@@ -12,11 +13,23 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
 
   const { name, email, password } = registerBodySchema.parse(request.body)
 
+  const passwordHash = await hash(password, 6)
+
+  const userWithSameEmail = await prisma.user.findUnique({
+    where: {
+      email,
+    },
+  })
+
+  if (userWithSameEmail) {
+    return reply.status(409)
+  }
+
   await prisma.user.create({
     data: {
       name,
       email,
-      password_hash: password,
+      password_hash: passwordHash,
     },
   })
 
