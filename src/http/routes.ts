25061709db1db1d910d9bcc6d776f4cfb6f0ea4b
ChristySchott <christyhauschild@gmail.com>
2025-05-15 18:07:03 -0300

    feat: implements jwt authentication

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -2,9 +2,13 @@ import { FastifyInstance } from 'fastify'
 
 import { register } from '@/http/controllers/register.controller.js'
 import { authenticate } from '@/http/controllers/authenticate.controller.js'
+import { profile } from '@/http/controllers/profile.js'
+
+import { verifyJWT } from '@/http/middlewares/verify-jwt.js'
 
 export async function appRoutes(app: FastifyInstance) {
   app.post('/users', register)
+  app.post('/authenticate', authenticate)
 
-  app.post('/sessions', authenticate)
+  app.get('/me', { onRequest: [verifyJWT] }, profile)
 }
