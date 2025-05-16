8412fafd4a9b16f321777cd61eb92eb8e508aba1
ChristySchott <christyhauschild@gmail.com>
2025-05-16 14:16:30 -0300

    test: add authenticate.spec.ts

---
+++ src/http/controllers/authenticate.spec.ts
@@ -0,0 +1,32 @@
+import supertest from 'supertest'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+
+import { app } from '@/app.js'
+
+describe('Authenticate (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to authenticate', async () => {
+    await supertest(app.server).post('/users').send({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '123456',
+    })
+
+    const response = await supertest(app.server).post('/authenticate').send({
+      email: 'johndoe@example.com',
+      password: '123456',
+    })
+
+    expect(response.statusCode).toEqual(200)
+    expect(response.body).toEqual({
+      token: expect.any(String),
+    })
+  })
+})
