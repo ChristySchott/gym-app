5d4af19096bcf8df4fa8e39b64d5e770f3727775
ChristySchott <christyhauschild@gmail.com>
2025-05-16 16:49:41 -0300

    fix: find nearby gyms query

--- src/repositories/prisma/prisma-gyms.repository.ts
+++ src/repositories/prisma/prisma-gyms.repository.ts
@@ -14,7 +14,7 @@ export class PrismaGymsRepository implements GymsRepository {
 
   async findManyNearby({ latitude, longitude }: FindManyNearbyParams): Promise<Gym[]> {
     const gyms = await prisma.$queryRaw<Gym[]>`
-      SELECT * from gyms where
+      SELECT * from gyms
       WHERE ( 6371 * acos( cos( radians(${latitude}) ) * cos( radians( latitude ) ) * cos( radians( longitude ) - radians(${longitude}) ) + sin( radians(${latitude}) ) * sin( radians( latitude ) ) ) ) <= 10
     `
 
